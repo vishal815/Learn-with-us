52678ba24a69ffdd9bc7db3cb7e578f50bbe9d1b
vishal lazrus <vishallazrus3210@gmail.com>
2023-08-30 21:03:31 +0530

    commite

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,5 @@
  import React from 'react'
- import Typed from 'react-typed';
+//  import Typed from 'react-typed';
  
  export default function Banner() {
    return (
@@ -12,12 +12,12 @@
 
        <h2 className='text-white font-bold text-5xl md:text-[80px] p-[24px]'>Grow with us.</h2>
        <div className='text-[20px] md:text-[50px] md:p-[20px] text-white my-4'>Learn 
-       <Typed
+       {/* <Typed
                     className='pl-3'
                     strings={['Machine Learning.', 'DevOps.', 'Web Development.']}                  //npm install react-typed --save     (React typed)
                     typeSpeed={120}
                     loop={true}
-                />
+                /> */}
        </div>
        <butten class="bg-black text-white p-3 rounded "> Get Started</butten>
        </div>
